ad6fd46831aa7a7e7dfd036da2a57152b3a9b728
philodoxos <chunsong.wang@gmail.com>
2023-10-01 22:36:46 +0800

    iter

--- src/animatediff/gene.py
+++ src/animatediff/gene.py
@@ -836,7 +836,7 @@ def run_inference(
     for clip_st in range(0, duration, context_frames-context_overlap):
         clip_en = min(duration, clip_st + context_frames)
         if clip_en - clip_st <= context_frames:
-            continue
+            break
 
         logger.info(f"Generating [{clip_st},{clip_en}] of size {width},{height}")
 
